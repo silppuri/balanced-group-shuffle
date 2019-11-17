0c095128d11417f31fe8abe650ffea5a86db6eb4
Petri Avikainen <petri.avikainen@gmail.com>
2019-11-17 19:39:34 +0200

    Let's make it "ugly" first

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,44 @@
 #[derive(Debug)]
 pub struct Shuffle {
     num_groups: usize,
-    total_items: usize
+    num_items: usize
 }
 
 impl Shuffle {
-    pub fn new(groups: Vec<Vec<usize>>) -> Shuffle {
-        Shuffle { num_groups: groups.len(), total_items: 0 }
+    pub fn new(groups: &Vec<Vec<usize>>) -> Shuffle {
+        let num_items = groups.iter().fold(0, |acc, group| acc + group.len());
+        let res: Vec<Vec<usize>> = groups.iter().enumerate().map(|group| {
+            let mut result_arr = vec![0; num_items];
+            let spread: usize = num_items / group.1.len();
+
+            // /*
+            // Create spread arrays with offset of the position of the 
+            // group in the initial argument. Also use modulus of the total
+            // amount of the items to make it a ring.
+            // [1, 0, 2, 0, 0]
+            // [0, 1, 0, 2, 0]
+            // [0, 0, 1, 0, 2]
+            // [2, 0, 0, 1, 0]
+            // */
+
+            group.1.iter().enumerate().for_each(|index_item_tuple| {
+                result_arr[(group.0 + index_item_tuple.0 * spread) % num_items] = *index_item_tuple.1;
+            });
+            result_arr
+        }).collect();
+
+        // Build the result array without the empty items
+        let result = (0..num_items).fold(Vec::with_capacity(num_items), |mut acc, index| {
+            res.iter().for_each(|item| {
+                if item[index] != 0 {
+                    println!("{}", item[index]);
+                    acc.push(item[index]);
+                }
+            });
+            acc
+        });
+        println!("{:?}", result);
+        Shuffle { num_groups: groups.len(), num_items: num_items }
     }
 }
 
@@ -23,6 +55,6 @@ fn test_data() -> Vec<Vec<usize>> {
 
 #[test]
 fn test_adder() {
-    assert!(Shuffle::new(test_data()).num_groups == 4);
-    assert!(Shuffle::new(test_data()).total_items == 12);
+    assert!(Shuffle::new(&test_data()).num_groups == 4);
+    assert!(Shuffle::new(&test_data()).num_items == 12);
 }
