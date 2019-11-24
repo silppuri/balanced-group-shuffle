403992d57b42993302f919cc31c4d087e750d872
Petri Avikainen <petri.avikainen@gmail.com>
2019-11-24 14:29:37 +0200

    Rename to BalancedGroupShuffle

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,16 @@
+use rand::prelude::*;
 use rand::{thread_rng, Rng};
 use std::hash::Hash;
 use std::collections::BTreeMap;
 
-pub struct GroupShuffle<K: Hash + Eq + Ord, T: Clone> {
+pub struct BalancedGroupShuffle<K: Hash + Eq + Ord, T: Clone> {
     groups: BTreeMap<K, Vec<T>>,
     num_values: usize
 }
 
-impl<K: Hash + Eq + Ord, T: Clone> GroupShuffle<K, T> {
-    pub fn new() -> GroupShuffle<K, T> {
-        GroupShuffle { groups: BTreeMap::new(), num_values: 0 }
+impl<K: Hash + Eq + Ord, T: Clone> BalancedGroupShuffle<K, T> {
+    pub fn new() -> BalancedGroupShuffle<K, T> {
+        BalancedGroupShuffle { groups: BTreeMap::new(), num_values: 0 }
     }
 
     pub fn len(&self) -> usize {
@@ -22,12 +23,11 @@ impl<K: Hash + Eq + Ord, T: Clone> GroupShuffle<K, T> {
 
     pub fn insert(&mut self, key: K, value: T) -> Option<Vec<T>> {   
         self.num_values += 1;
-        GroupShuffle::value_into_map_vector(&mut self.groups, key, value)
+        BalancedGroupShuffle::value_into(&mut self.groups, key, value)
     }
 
     pub fn shuffle(&self) -> Vec<&T> {
-        let positioned_item_groups = self.disperse_values();
-        positioned_item_groups.iter().flat_map(|(_position, values)| values).cloned().collect()
+        self.disperse_values().iter().flat_map(|(_position, values)| values).cloned().collect()
     }
 
     fn disperse_values(&self) -> BTreeMap<usize, Vec<&T>> {
@@ -36,7 +36,7 @@ impl<K: Hash + Eq + Ord, T: Clone> GroupShuffle<K, T> {
             let spread = self.num_values / values.len();
             for (j, value) in values.iter().enumerate() {
                 let position = self.position(j, spread);
-                GroupShuffle::value_into_map_vector(&mut map, position, value);
+                BalancedGroupShuffle::value_into(&mut map, position, value);
             }
         }
         map
@@ -44,21 +44,24 @@ impl<K: Hash + Eq + Ord, T: Clone> GroupShuffle<K, T> {
 
     fn position(&self, j: usize, spread: usize) -> usize {
         let mut rng = thread_rng();
+        let randomness = 0.05;
         if j == 0 {
             rng.gen_range(0, self.len())
         } else {
-            let lower = spread as f64 * j as f64 - self.num_values as f64 * 0.05;
-            let upper = spread as f64 * j as f64 + self.num_values as f64 * 0.05;
+            let lower = spread as f64 * j as f64 - spread as f64 * randomness;
+            let upper = spread as f64 * j as f64 + spread as f64 * randomness;
             rng.gen_range(lower, upper) as usize
         }
     }
 
-    fn value_into_map_vector(map: &mut BTreeMap<K, Vec<T>>, key: K, value: T) -> Option<Vec<T>> {
+    fn value_into(map: &mut BTreeMap<K, Vec<T>>, key: K, value: T) -> Option<Vec<T>> {
         match map.get_mut(&key) {
             Some(values) => {
+                let mut rng = thread_rng();
                 let mut new_values = vec![];
                 new_values.append(values);
                 new_values.append(&mut vec![value]);
+                new_values.shuffle(&mut rng);
                 map.insert(key, new_values)
             }
             None => {
@@ -69,9 +72,8 @@ impl<K: Hash + Eq + Ord, T: Clone> GroupShuffle<K, T> {
 }
 
 #[cfg(test)]
-#[test]
-fn test_adder() {
-    let mut shuffle = GroupShuffle::new();
+fn test_data() -> BalancedGroupShuffle<&'static str, &'static str> {
+    let mut shuffle = BalancedGroupShuffle::new();
     shuffle.insert("a", "a1");
     shuffle.insert("a", "a2");
     shuffle.insert("b", "b1");
@@ -84,8 +86,18 @@ fn test_adder() {
     shuffle.insert("d", "d1");
     shuffle.insert("d", "d2");
     shuffle.insert("d", "d3");
+    shuffle
+}
+
+#[test]
+fn test_insert() {
+    let shuffle = test_data();
     assert!(shuffle.len() == 4);
     assert!(shuffle.values().len() == 12);
+}
+
+#[test]
+fn test_shuffle() {
+    let shuffle = test_data();
     assert!(shuffle.shuffle().len() == 12);
-    println!("{:?}", shuffle.shuffle());
 }
